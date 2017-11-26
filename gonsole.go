ae3127c1beac845c704d27ae77ea176b16ff83d4
Steven Ferrer <steven.r.ferrer@gmail.com>
2017-11-26 17:28:41 +0800

    added gonsole.go, gonsole_test.go, examples/main.go

---
+++ gonsole.go
@@ -0,0 +1,123 @@
+package gonsole
+
+import (
+	"bufio"
+	"io"
+	"strconv"
+	"strings"
+)
+
+//Reader is a wrapper for *bufio.Reader
+type Reader struct {
+	reader *bufio.Reader
+}
+
+//Int32 reads the next line and tries
+//to convert it to int32
+func (r *Reader) Int32() (int32, error) {
+	s, err := r.Line()
+	if err != nil {
+		return 0, err
+	}
+
+	i32, err := strconv.ParseInt(s, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(i32), nil
+}
+
+//Uint32 reads the next line and tries
+//to convert it to uint32
+func (r *Reader) Uint32() (uint32, error) {
+	s, err := r.Line()
+	if err != nil {
+		return 0, err
+	}
+
+	ui32, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	return uint32(ui32), nil
+}
+
+//Int32 reads the next line and tries
+//to convert it to int64
+func (r *Reader) Int64() (int64, error) {
+	s, err := r.Line()
+	if err != nil {
+		return 0, err
+	}
+
+	i64, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return i64, nil
+}
+
+//Uint64 reads the next line and tries
+//to converts it to uint64
+func (r *Reader) Uint64() (uint64, error) {
+	s, err := r.Line()
+	if err != nil {
+		return 0, err
+	}
+
+	ui64, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return ui64, nil
+}
+
+//Float32 reads the next line and tries
+//to convert it to float32
+func (r *Reader) Float32() (float32, error) {
+	s, err := r.Line()
+	if err != nil {
+		return 0, err
+	}
+
+	f32, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return float32(f32), nil
+}
+
+//Float64 reads the next line and tries
+//to convert it to float64
+func (r *Reader) Float64() (float64, error) {
+	s, err := r.Line()
+	if err != nil {
+		return 0, err
+	}
+
+	f64, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return f64, nil
+}
+
+//Line reads the next line (terminated by newline) and trims
+//the extra spaces around including newline
+func (r *Reader) Line() (string, error) {
+	s, err := r.reader.ReadString('\n')
+	//remove spaces and extra lines
+	s = strings.TrimSpace(s)
+	return s, err
+}
+
+//NewReader takes an io.Reader returns a new console reader
+func NewReader(rd io.Reader) *Reader {
+	r := bufio.NewReader(rd)
+	return &Reader{r}
+}
---
+++ gonsole_test.go
@@ -0,0 +1,147 @@
+package gonsole
+
+import (
+	"math"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+//TODO: Test for reading from file
+
+func TestInt32InRange(t *testing.T) {
+	s := strconv.FormatInt(math.MaxInt32, 10) + "\n"
+	reader := NewReader(strings.NewReader(s))
+
+	_, err := reader.Int32()
+	if err != nil {
+		t.Error("max: expecting nil error got", err)
+	}
+
+	s = strconv.FormatInt(math.MinInt32, 10) + "\n"
+	reader = NewReader(strings.NewReader(s))
+	_, err = reader.Int32()
+	if err != nil {
+		t.Error("min: expecting nil error got", err)
+	}
+}
+
+func TestInt32OutOfRange(t *testing.T) {
+	s := strconv.FormatInt(math.MaxInt32+1, 10) + "\n"
+	reader := NewReader(strings.NewReader(s))
+	_, err := reader.Int32()
+	if err == nil {
+		t.Error("max: expecting non-nil error got ", err)
+	}
+
+	s = strconv.FormatInt(math.MinInt32-1, 10) + "\n"
+	reader = NewReader(strings.NewReader(s))
+	_, err = reader.Int32()
+	if err == nil {
+		t.Error("min: expecting non-nil error got ", err)
+	}
+}
+
+func TestUint32InRange(t *testing.T) {
+	s := strconv.FormatUint(math.MaxUint32, 10) + "\n"
+	reader := NewReader(strings.NewReader(s))
+	_, err := reader.Uint32()
+	if err != nil {
+		t.Error("max: expecting nil error got", err)
+	}
+
+	s = strconv.FormatUint(0, 10) + "\n"
+	reader = NewReader(strings.NewReader(s))
+	_, err = reader.Uint32()
+	if err != nil {
+		t.Error("min: expecting nil error got", err)
+	}
+}
+
+func TestUint32OutOfRange(t *testing.T) {
+	s := strconv.FormatUint(math.MaxUint32+1, 10) + "\n"
+	reader := NewReader(strings.NewReader(s))
+	_, err := reader.Uint32()
+	if err == nil {
+		t.Error("Expecting non-nil error got ", err)
+	}
+}
+
+func TestInt64InRange(t *testing.T) {
+	s := strconv.FormatInt(math.MaxInt64, 10) + "\n"
+	reader := NewReader(strings.NewReader(s))
+
+	_, err := reader.Int64()
+	if err != nil {
+		t.Error("max: expecting nil error got", err)
+	}
+
+	s = strconv.FormatInt(math.MinInt64, 10) + "\n"
+	reader = NewReader(strings.NewReader(s))
+	_, err = reader.Int64()
+	if err != nil {
+		t.Error("min: expecting nil error got", err)
+	}
+}
+
+func TestUint64InRange(t *testing.T) {
+	s := strconv.FormatUint(math.MaxUint64, 10) + "\n"
+	reader := NewReader(strings.NewReader(s))
+
+	_, err := reader.Uint64()
+	if err != nil {
+		t.Error("max: expecting nil error got", err)
+	}
+
+	s = strconv.FormatUint(0, 10) + "\n"
+	reader = NewReader(strings.NewReader(s))
+	_, err = reader.Uint64()
+	if err != nil {
+		t.Error("min: expecting nil error got", err)
+	}
+}
+
+func TestFloat32InRange(t *testing.T) {
+	s := strconv.FormatFloat(math.MaxFloat32, 'E', -1, 32) + "\n"
+	reader := NewReader(strings.NewReader(s))
+	_, err := reader.Float32()
+	if err != nil {
+		t.Error("expecting a nil error got", err)
+	}
+
+	s = strconv.FormatFloat(math.SmallestNonzeroFloat32, 'E', -1, 32) + "\n"
+	reader = NewReader(strings.NewReader(s))
+	_, err = reader.Float32()
+	if err != nil {
+		t.Error("expecting a nil error got", err)
+	}
+}
+
+func TestFloat64InRange(t *testing.T) {
+	s := strconv.FormatFloat(math.MaxFloat64, 'E', -1, 64) + "\n"
+	reader := NewReader(strings.NewReader(s))
+	_, err := reader.Float64()
+	if err != nil {
+		t.Error("expecting a nil error got", err)
+	}
+
+	s = strconv.FormatFloat(math.SmallestNonzeroFloat64, 'E', -1, 64) + "\n"
+	reader = NewReader(strings.NewReader(s))
+	_, err = reader.Float64()
+	if err != nil {
+		t.Error("expecting a nil error got", err)
+	}
+}
+
+func TestLine(t *testing.T) {
+	s := "The quick brown fox jumps over the lazy dog.\n"
+	reader := NewReader(strings.NewReader(s))
+	s2, err := reader.Line()
+	if err != nil {
+		t.Error("expecting nil error got", err)
+	}
+
+	if strings.TrimSpace(s) != s2 {
+		t.Errorf("%q is not equal to %q", s, s2)
+	}
+}
